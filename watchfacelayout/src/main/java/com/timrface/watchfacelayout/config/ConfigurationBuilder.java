3bcce0ec6c65f5fb25efa49415485e2d6f0f6a5e
Florian Möhle <florian.moehle@itelligence.de>
2020-10-31 13:58:38 +0100

    Remove old methods calls

--- watchfacelayout/src/main/java/com/timrface/watchfacelayout/config/ConfigurationBuilder.java
+++ watchfacelayout/src/main/java/com/timrface/watchfacelayout/config/ConfigurationBuilder.java
@@ -8,14 +8,12 @@ public class ConfigurationBuilder {
 
     public static Configuration getDefaultConfiguration(Context context) {
         return new Configuration()
-                .setShowBatteryLevel(true)
                 .setSmoothScrolling(true)
                 .setBackgroundColor(Color.parseColor("#FAFAFA"))
                 .setInteractiveColor(Color.parseColor("#FF9800"))
                 .setTextColor(Color.parseColor("#424242"))
                 .setShowZeroDigit(true)
                 .setAstronomicalClockFormat(DateFormat.is24HourFormat(context))
-                .setShowUnreadNotificationsCounter(true)
                 .setUseStrokeDigitsInAmbientMode(false)
                 .setAutomaticDarkLightMode(false)
                 .setLeftComplicationType(ComplicationType.BATTERY)
